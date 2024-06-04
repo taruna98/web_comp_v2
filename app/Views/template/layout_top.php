4d8ca7ee4d618bc8fb54eb18305abe417b977d53
taruna ersa <jhonny.ocnr@gmail.com>
2024-06-04 23:23:22 +0700

    setup assets and minor setup for web kretech id

--- app/Views/template/layout_top.php
+++ app/Views/template/layout_top.php
@@ -5,12 +5,13 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title><?= $_ENV['APP_NAME'] ?></title>
+  <title>Kretech ID</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <!-- <link href="assets/img/favicon.png" rel="icon"> -->
+  <link href="assets/img/logo-kretech.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
